6265dddce450d1ae71dd0e1ec33ff63ef9b21df7
iMykhailychenko <igor.c.m@ukr.net>
2021-07-04 15:08:38 +0300

    general commit

--- src/components/common/image-upload/index.tsx
+++ src/components/common/image-upload/index.tsx
@@ -1,11 +1,15 @@
 import { observer } from 'mobx-react';
 import React, { ReactElement, useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ActionSheetIOS, Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ImagePickerResponse, launchImageLibrary } from 'react-native-image-picker';
+import { launchCamera } from 'react-native-image-picker/src/index';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { useColors } from '../../../hooks/colors.hook';
+import { useImageUpload } from '../../../hooks/image-upload.hook';
+import { useStore } from '../../../hooks/store.hook';
 import { Navigation } from '../../../interfaces';
-import { SelectedPhoto } from '../modals/select-photo';
+import { IUpload } from '../../../store/upload';
 
 interface IProps {
     navigation: Navigation;
@@ -13,13 +17,55 @@ interface IProps {
 
 export const ImgUpload = observer(({ navigation }: IProps): ReactElement => {
     const colors = useColors();
+    const imageUpload = useImageUpload();
+    const upload = useStore<IUpload>(state => state.upload);
 
-    const [visible, setVisible] = useState<boolean>(false);
-    const toggle = (): void => setVisible(prev => !prev);
+    const handleCamera = async () => {
+        launchCamera({ mediaType: 'photo', cameraType: 'front' }, async (response: ImagePickerResponse): Promise<void> => {
+            if (response?.assets?.[0]?.uri) {
+                const uri = await imageUpload(response?.assets?.[0]?.uri);
+                await upload.add(uri);
+                navigation.navigate('Tags');
+            } else {
+                Alert.alert('Camera unavailable');
+            }
+        });
+    };
+
+    const handleGallery = async () => {
+        launchImageLibrary({ mediaType: 'photo' }, async (response: ImagePickerResponse): Promise<void> => {
+            if (response?.assets?.[0]?.uri) {
+                const uri = await imageUpload(response?.assets?.[0]?.uri);
+                await upload.add(uri);
+                navigation.navigate('Tags');
+            } else {
+                Alert.alert('Gallery unavailable');
+            }
+        });
+    };
+
+    const toggle = (): void => {
+        ActionSheetIOS.showActionSheetWithOptions(
+            {
+                options: ['Cancel', 'Take a photo', 'Select from gallery'],
+                cancelButtonIndex: 0,
+            },
+            async index => {
+                switch (index) {
+                    case 1:
+                        await handleCamera();
+                        break;
+
+                    case 2:
+                        await handleGallery();
+                        break;
+                }
+            },
+        );
+    };
 
     return (
         <>
-            <SelectedPhoto visible={visible} onClose={toggle} navigation={navigation} />
             <TouchableOpacity onPress={toggle}>
                 <View style={{ ...styles.button, backgroundColor: colors.red700 }}>
                     <Icon name="image-search" style={styles.icon} />
