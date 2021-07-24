7bb5065d0824109214bf582ec7b436bb48908397
iMykhailychenko <igor.c.m@ukr.net>
2021-07-24 12:42:26 +0300

    general commit

--- src/components/common/share-button/index.tsx
+++ src/components/common/share-button/index.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement } from 'react';
-import { Alert, Share, StyleSheet, Text } from 'react-native';
+import { Alert, Platform, StyleSheet, Text } from 'react-native';
+import Share from 'react-native-share';
+import { ShareOptions } from 'react-native-share/src/types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { useColors } from '../../../hooks/colors.hook';
@@ -10,28 +12,71 @@ interface IProps {
     tags: IAdaptedHashtag[];
 }
 
-export const ShareButton = ({ file, tags }: IProps): ReactElement => {
+export const ShareButton = ({ file: url, tags }: IProps): ReactElement => {
     const colors = useColors();
+
+    const title = 'title';
+    const icon = 'data:<data_type>/<file_extension>;base64,<base64_data>';
+    const message = tags.reduce((acc, item) => (acc += item.active ? ` #${item.tag}` : ''), '')?.trim();
+
+    const options = Platform.select({
+        ios: {
+            activityItemSources: [
+                {
+                    // For sharing url with custom title.
+                    placeholderItem: { type: 'url', content: url },
+                    item: {
+                        default: { type: 'url', content: url },
+                    },
+                    subject: {
+                        default: title,
+                    },
+                    linkMetadata: { originalUrl: url, url, title },
+                },
+                {
+                    // For sharing text.
+                    placeholderItem: { type: 'text', content: message },
+                    item: {
+                        default: { type: 'text', content: message },
+                        message: null, // Specify no text to share via Messages app.
+                    },
+                    linkMetadata: {
+                        // For showing app icon on share preview.
+                        title: message,
+                    },
+                },
+                {
+                    // For using custom icon instead of default text icon at share preview when sharing with message.
+                    placeholderItem: {
+                        type: 'url',
+                        content: icon,
+                    },
+                    item: {
+                        default: {
+                            type: 'text',
+                            content: `${message} ${url}`,
+                        },
+                    },
+                    linkMetadata: {
+                        title: message,
+                        icon: icon,
+                    },
+                },
+            ],
+        },
+        default: {
+            title,
+            url,
+            message,
+            subject: title,
+        },
+    });
 
     const handleClick = async (): Promise<void> => {
         try {
-            const result = await Share.share({
-                url: file,
-                title: tags.reduce((acc, item) => (acc += item.active ? ` #${item.tag}` : ''), '')?.trim(),
-                message: tags.reduce((acc, item) => (acc += item.active ? ` #${item.tag}` : ''), '')?.trim(),
-            });
-
-            if (result.action === Share.sharedAction) {
-                if (result.activityType) {
-                    // shared with activity type of result.activityType
-                } else {
-                    // shared
-                }
-            } else if (result.action === Share.dismissedAction) {
-                // dismissed
-            }
+            await Share.open(options as ShareOptions);
         } catch (error) {
-            Alert.alert(error.message);
+            Alert.alert(error?.message);
         }
     };
 
